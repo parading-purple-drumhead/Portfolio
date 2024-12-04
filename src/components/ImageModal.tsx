3d3fef99b4d16e2d9731eee9a2a75fe1ca1a3a5e
Sudhanshu Basu Roy <sbasur2@uic.edu>
2024-12-04 04:22:53 -0800

    Image modal solid background

--- src/components/ImageModal.tsx
+++ src/components/ImageModal.tsx
@@ -23,12 +23,12 @@ const ImageModal = ({ open, children, onClose }: ModalProps) => {
   return createPortal(
     <div className="portal">
       <div
-        className="overlay fixed bottom-0 left-0 right-0 top-0 z-10 bg-black/70"
+        className="overlay fixed bottom-0 left-0 right-0 top-0 z-10 bg-black"
         onClick={onClose}
       ></div>
       <div
         className={
-          "modal fixed left-1/2 top-1/2 z-10 max-h-[90vh] w-[90vw] -translate-x-1/2 -translate-y-1/2 overflow-y-scroll rounded bg-white lg:w-2/3"
+          "modal fixed left-1/2 top-1/2 z-10 max-h-[90vh] w-[90vw] -translate-x-1/2 -translate-y-1/2 touch-manipulation overflow-y-scroll rounded bg-white lg:w-2/3"
         }
       >
         <button
