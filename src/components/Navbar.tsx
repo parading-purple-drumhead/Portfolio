83747dab2ebadc9477392423b20d350bb15630c1
Sudhanshu Basu Roy <sbasur2@uic.edu>
2024-12-04 03:27:37 -0800

    Hamburger icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -90,7 +90,20 @@ const Navbar = () => {
                 setIsDropdownOpen((prev) => !prev);
               }}
             >
-              Hamburger
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-8"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+              </svg>
             </button>
           </div>
         </div>
